5d5eef9d49f2b8a26d16f3fdd37a73db39444b1a
jack <56966366+jixinliang@users.noreply.github.com>
2022-11-19 13:14:23 +0000

    add some feature

--- rust_demo/src/main.rs
+++ rust_demo/src/main.rs
@@ -3,21 +3,28 @@ use std::sync::atomic::{AtomicU64, Ordering::Relaxed};
 use std::thread;
 use std::time::Instant;
 
-static A: AtomicU64 = AtomicU64::new(0);
+// 该结构体必须是64字节对齐的
+#[repr(align(64))]
+struct Aligned(AtomicU64);
+
+static A: [Aligned; 3] = [
+    Aligned(AtomicU64::new(0)),
+    Aligned(AtomicU64::new(0)),
+    Aligned(AtomicU64::new(0)),
+];
 
 fn main() {
     black_box(&A);
 
-    // New!
     thread::spawn(|| loop {
-        // New!
-        black_box(A.store(0, Relaxed));
+        A[0].0.store(0, Relaxed);
+        A[2].0.store(0, Relaxed);
     });
 
     let start = Instant::now();
 
-    for _ in 0..1_000_000_000 {
-        black_box(A.load(Relaxed));
+    for _ in 0..100_000_000 {
+        black_box(A[1].0.load(Relaxed));
     }
 
     println!("{:?}", start.elapsed());
